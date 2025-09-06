2a4e517dc7dd8bdcce67975872c557ad63333951
asa129 <asa.jad1290@gmail.com>
2025-09-06 12:22:24 +0900

    Veliteライブラリを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { posts } from "../../.velite";
 
 export default function Home() {
+  console.log(posts);
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
